6b107ce73353e7632cb301aec476e7fcd2d3d03b
Wicker25 <wicker25@gmail.com>
2025-07-16 16:57:55 +0100

    Updated `DeliveryApi`

--- webapp-ui/src/apis/DeliveryApi.js
+++ webapp-ui/src/apis/DeliveryApi.js
@@ -10,15 +10,4 @@ export class DeliveryApi extends ApiClient {
   async logout() {
     await this.axiosInstance.get('/v1/account/logout');
   }
-
-  async register(firstName, lastName, email, password) {
-    const response = await this.axiosInstance.post('/v1/account/register', {
-      firstName,
-      lastName,
-      email,
-      password,
-    });
-
-    return response.data;
-  }
 }
